2c2cf0d7a912fea29b2d0465fa054de256e615ad
Priyanshu <priyanshupareta987@gmail.com>
2022-08-21 08:10:16 +0530

    Team API and Batch

---
+++ src/main/java/com/ipldashboard/ipldashboard/controller/TeamController.java
@@ -0,0 +1,39 @@
+package com.ipldashboard.ipldashboard.controller;
+
+import com.ipldashboard.ipldashboard.model.Team;
+import com.ipldashboard.ipldashboard.repository.MatchRepository;
+import com.ipldashboard.ipldashboard.repository.TeamRepository;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TeamController {
+    
+    private TeamRepository teamRepository;
+    private MatchRepository matchRepository;
+
+    
+
+    public TeamController(TeamRepository teamRepository, MatchRepository matchRepository) {
+        this.teamRepository = teamRepository;
+        this.matchRepository = matchRepository;
+    }
+
+
+
+    @GetMapping("/team/{teamName}")
+    public Team getTeam(@PathVariable String teamName){
+        Team team = this.teamRepository.findByTeamName(teamName);
+
+
+        team.setMatches(matchRepository.findLatestMatchesbyTeam(teamName, 4));
+
+        return team;
+    }
+
+    
+    
+
+}
---
+++ src/main/java/com/ipldashboard/ipldashboard/repository/TeamRepository.java
@@ -0,0 +1,9 @@
+package com.ipldashboard.ipldashboard.repository;
+
+import com.ipldashboard.ipldashboard.model.Team;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface TeamRepository extends CrudRepository<Team, Long> {
+    Team findByTeamName(String teamName);
+}
